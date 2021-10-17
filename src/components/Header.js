1665cc4506ad05ed20d6efd18bd433b599ce7d0b
rpgarde <rpgarde@gmail.com>
2021-10-17 15:21:36 +1100

    basic page switcher working

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,38 +4,47 @@ import React from 'react';
 // We assign them to their own variable names
 
 
-function Header({currentPage, handlePageChange}){
+function Header({ currentPage, handlePageChange }) {
   return (
     <nav className="navbar navbar-expand-md bg-dark navbar-dark">
-    <div className="container-fluid">
-      <a className="navbar-brand fw-bold" href = "#about">paolo garde.</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a aria-current="page" href="#about"
-                      className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-                      onClick={() => handlePageChange('About')}
-            >
-              about me
+      <div className="container-fluid">
+        <a className="navbar-brand fw-bold" href="#about">paolo garde.</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a aria-current="page" href="#about"
+                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                onClick={() => handlePageChange('About')}
+              >
+                about me
               </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#portfolio">my portfolio</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#resume">my resume</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#contact">contact me</a>
-          </li>
-        </ul>
+            </li>
+            <li className="nav-item">
+              <a aria-current="page" href="#portfolio"
+                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                onClick={() => handlePageChange('Portfolio')}
+              >
+                my portfolio
+              </a>
+            </li>          <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#resume">my resume</a>
+            </li>
+            <li className="nav-item">
+              <a aria-current="page" href="#contact"
+                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                onClick={() => handlePageChange('Contact')}
+              >
+                contact me
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
   )
 }
 
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import Header from './Header';
-import Home from './pages/Home';
 import About from './pages/About';
-import Blog from './pages/Blog';
+import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
@@ -10,16 +9,13 @@ export default function PortfolioContainer() {
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
     if (currentPage === 'About') {
       return <About />;
     }
-    if (currentPage === 'Blog') {
-      return <Blog />;
+    if (currentPage === 'Contact') {
+      return <Contact />;
     }
-    return <Contact />;
+    return <Portfolio />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
