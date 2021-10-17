01669bb78f4a8045c7851f7c5eb0f65938dc792f
rpgarde <rpgarde@gmail.com>
2021-10-17 17:02:09 +1100

    basic footer styling with links done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import github from './images/github-icon.png'
+import linkedin from './images/linkedin-icon.png'
+import youtube from './images/youtube-icon.png'
 
-export default function Footer () {
-   return (
-        <footer className="footer bg-dark py-3 mt-auto">
+const style = {
+    maxWidth: '50px'
+}
+export default function Footer() {
+    return (
+        <footer className="footer bg-dark mt-auto py-4">
+            <div className="d-flex justify-content-center py-2">
+                <a className = "mx-2" href="htts://www.github.com/rpgarde">
+                    <img src={github} alt="github" style={style} />
+                </a>
+                <a className = "mx-2" href="htts://www.linkedin.com/in/paologarde">
+                    <img src={linkedin} alt="linkedin" style={style} />
+                </a>
+                <a className = "mx-2" href="htts://www.youtube.com/pgblanks">
+                    <img src={youtube} alt="youtube" style={style} />
+                </a>
+            </div>
             <div className="container d-flex justify-content-center">
-                <span className="text-muted">Paolo Garde, 2021</span>
+                <span className="text-muted">&copy; Paolo Garde, 2021</span>
             </div>
         </footer>
     )
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,17 +1,25 @@
 import React from 'react';
-
+import profile from '../images/profile-pic.png'
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <div className = "row">
+      <div>
+        <div className="card mb-3 border-0">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={profile} className="img-fluid rounded-start" alt="paolo garde"/>
+            </div>
+            <div className="card-body col-md-8">
+              <div className="my-4">
+                <h1>Hi 👋, I'm Paolo.</h1>
+                <p className = "my-2">I'm a web developer with over 8 years of Ops leadership experience in
+                  startups like Uber, FoodByUs, and :Different.</p>
+                <p className = "my-2"> I'm finishing up a Coding Bootcamp at The University of Sydney, graduating November 2021. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>    
+      </div>
   );
 }
